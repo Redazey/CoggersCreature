3316a24ff3f06a95d933c51fb6e04da2efd8ad7f
Redazey <w.d.gaster00@mail.ru>
2024-03-02 19:47:50 +0300

    basa

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Person:
     def __init__(self, start_x, start_y):
         self.x = start_x
         self.y = start_y
+
         # инициализируем нашего персонажа на начальных координатах, данных при инициализации класса
         self.pers = canvas.create_oval(self.x, self.y, self.x + 100, self.y + 100, fill='black')
 
@@ -22,11 +23,11 @@ class Person:
 
     # Смещение персонажа направо или налево
     def move(self, direction):
-        if direction == "right" and self.x < win.winfo_screenheight():
+        if direction == "right" and self.x + 100 < win.winfo_screenwidth():
             self.x += 5
             canvas.coords(self.pers, self.x, self.y, self.x + 100, self.y + 100)
 
-        elif direction == "left":
+        elif direction == "left" and self.x > win.winfo_screenwidth():
             self.x -= 5
             canvas.coords(self.pers, self.x, self.y, self.x + 100, self.y + 100)
         else:
@@ -34,21 +35,23 @@ class Person:
 
 
 def update(e=None):
-    # здесь будут происходить события с нашим персонажем
-    ralsei.move("right")
+    # Здесь будут происходить события с нашим персонажем
+    mox.move("right")
+    win.after(int(1000 / FPS), update)
 
 
 FPS = 30
 
 # Создаем Canvas, на котором будет перемещаться персонаж и заливаем его прозрачным цветом
-
 canvas = tk.Canvas(win, bg='white', bd=0, highlightthickness=0)
 canvas.place(x=0, y=0, width=win.winfo_screenwidth(), height=win.winfo_screenheight())
 win.overrideredirect(True)
 win.state('zoomed')
 win.wm_attributes("-topmost", True)
 win.wm_attributes("-transparentcolor", "white")
-ralsei = Person(1, 1)
+
+# Создаем персонажа на определенных координатах
+mox = Person(1, win.winfo_screenheight() - 100)
 
 # Зацикливаем нашу программу с фиксированным FPS
 if __name__ == '__main__':
