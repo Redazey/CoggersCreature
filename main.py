24ac29fe781b4459943056b365450d6540ffecf2
Redazey <w.d.gaster00@mail.ru>
2024-03-04 23:24:49 +0300

    Функция Pers_action как пресет действий, которые может совершать персонаж, + допилил до адеквата физику и убрал баг с прыжком, когда он останавливался в воздухе. Добавил маленькую анимацию для хождения вправо, в качестве теста

--- main.py
+++ main.py
@@ -8,27 +8,29 @@ class Person:
     def __init__(self, start_x, start_y):
         self.x = start_x
         self.y = start_y
-        self.width = 400
-        self.height = 400
-
-        # Задаем персонажу его спрайт по пути файла спрайта
-        # Инициализируем нашего персонажа на начальных координатах, данных при инициализации класса
+        self.width = 200
+        self.height = 200
+        """
+        Задаем персонажу его спрайт по пути файла спрайта
+        инициализируем нашего персонажа на начальных координатах, данных при инициализации класса
+        """
         self.current_sprite = None
         self.pers = canvas.create_image(self.x, self.y, anchor='nw', image=self.current_sprite)
-        self.changeCurrentSprite(r"sprites\MoxStand.png")
+        self.change_current_sprite(r"sprites\MoxStand.png")
 
         # Все, что связано с действиями персонажа
-        self.action_list = [moveRight, moveLeft, jumpRight, jumpLeft] # Нужен для случайного выбора действия
-        self.Test_action_list = [moveRight]
+        # массив нужен для случайного выбора действия
+        self.action_list = ["moveRight", "moveLeft", "jumpRight", "jumpLeft"]
         self.action = None
         self.action_count = 0
         self.impulse_x = 0
         self.impulse_y = 0
 
     # Создает объект картинки по строке пути картинки
-    def changeCurrentSprite(self, path):
+    def change_current_sprite(self, path):
         self.current_sprite = ImageTk.PhotoImage((Image.open(path).resize((self.width, self.height))))
         canvas.itemconfigure(self.pers, image=self.current_sprite)
+
     # Она возвращает координаты нашего персонажа
     def get_cords(self):
         return self.x, self.x + self.width, self.y, self.y + self.height
@@ -40,10 +42,6 @@ class Person:
     def set_impulse(self, impulse_x, impulse_y):
         self.impulse_y = impulse_y
         self.impulse_x = impulse_x
-
-    def change_impulse(self, impulse_x, impulse_y):
-        self.impulse_y += impulse_y
-        self.impulse_x += impulse_x
 
     # Смещение персонажа направо, налево и по вертикали
     def move(self, direction, speed):
@@ -60,13 +58,11 @@ class Person:
             self.x = 1
 
         # перемещение по вертикали
-        self.y = self.y + self.height
         if direction == "vertical" and self.y + self.height + speed <= max_y:
             self.y += speed
         # доводит положение персонажа до нижнего края, но не дает выйти за него, за верхний выходить можно
         elif direction == "vertical" and self.y + self.height + speed > max_y:
             self.y = max_y - self.height
-
         else:
             return 'Direction can accept only "right"/"left/vertical" arguments'
 
@@ -94,61 +90,61 @@ class Person:
         # не полетел в обратную сторону
         elif self.impulse_x - x_gravity < 0 and self.y + self.height < max_y:
             self.impulse_x = 0
-def beIdle():
-    mox.action = "doNothing"
-    mox.set_impulse(0, 0)
-    mox.action_count -= 2
-def moveRight():
-    mox.action = "moveRight"
-    mox.changeCurrentSprite("sprites/MoxRight.png")
-    mox.set_impulse(5, 0)
-    mox.action_count -= 4
-def moveLeft():
-    mox.action = "moveLeft"
-    mox.changeCurrentSprite("sprites/MoxLeft.png")
-    mox.set_impulse(-5, 0)
-    mox.action_count -= 4
-def jumpRight():
-    mox.action = "jumpRight"
-    mox.change_impulse(5, -25)
-    mox.action_count -= 25
-def jumpLeft():
-    mox.action = "jumpLeft"
-    mox.change_impulse(-5, -25)
-    mox.action_count -= 25
+
+
+# набор пресетов для движения
+def pers_action(action):
+    action_weight = 0
+    match action:
+        case "moveLeft":
+            mox.change_current_sprite("sprites/MoxLeft.png")
+            mox.set_impulse(-5, 0)
+            action_weight = 5
+        case "moveRight":
+            if mox.action_count % 3 == 0:
+                mox.change_current_sprite("sprites/MoxRight_stepOne.jpg")
+            else:
+                mox.change_current_sprite("sprites/MoxRight_stepTwo.jpg")
+            mox.set_impulse(5, 0)
+            action_weight = 5
+        case "jumpLeft":
+            mox.change_current_sprite("sprites/MoxLeft.png")
+            mox.set_impulse(-5, -15)
+            action_weight = 100
+            win.after(100)
+        case "jumpRight":
+            mox.change_current_sprite("sprites/MoxRight.png")
+            mox.set_impulse(5, -15)
+            mox.action_count = 200
+            win.after(100)
+        case "idle":
+            mox.change_current_sprite("sprites/MoxStand.png")
+            mox.set_impulse(0, 0)
+            action_weight = 2
+    mox.action_count -= action_weight
+
 
 def update():
     x_speed, y_speed = mox.get_speed()
-    x1, x2, y1, y2 = mox.get_cords()
     mox.move("vertical", y_speed)
     mox.move("horizontal", x_speed)
     mox.inertia()
+    x1, x2, y1, y2 = mox.get_cords()
 
     # Здесь будут происходить события с нашим персонажем, если он стоит на земле
     if y2 >= max_y:
         if mox.action_count <= 0:
-            random.choice(mox.action_list)()
+            mox.action = random.choice(mox.action_list)
             mox.action_count = random.randint(300, 500)
             win.after(1000 // FPS, update)
 
         # Делает так, чтобы персонаж стоял на месте между действиями.
         elif mox.action_count <= 200:
-            beIdle()
+            pers_action("idle")
             win.after(1000 // FPS, update)
         else:
-            match mox.action:
-                case "moveRight":
-                    moveRight()
-                    win.after(1000 // FPS, update)
-                case "moveLeft":
-                    moveLeft()
-                    win.after(1000 // FPS, update)
-                case "jumpRight":
-                    jumpRight()
-                    win.after(1000 // FPS, update)
-                case "jumpLeft":
-                    jumpLeft()
-                    win.after(1000 // FPS, update)
+            pers_action(mox.action)
+            win.after(1000 // FPS, update)
     else:
         win.after(1000 // FPS, update)
 
