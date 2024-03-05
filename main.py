6a00472beaa42d0280d8d2ce976bb6eaedc1f2fe
Redazey <w.d.gaster00@mail.ru>
2024-03-05 19:25:26 +0300

    Рефактор кода

--- main.py
+++ main.py
@@ -4,8 +4,54 @@ from PIL import ImageTk, Image
 win = tk.Tk()
 
 
+class Sprite:
+    def __init__(self, path: str, duration, width, height):
+        self.sprite = ImageTk.PhotoImage(Image.open(path).resize((width, height)))
+        # Длительность расчитывается по формуле "длительность в секундах / (1 секунда / FPS)",
+        # итого получаем длительность в кадрах
+        # i.e. 5sec / (1sec / 25FPS) = 125 frames
+        self.max_duration = duration / (1 / FPS)
+        self.current_duration = 0
+
+    # Обновляет длительность этого кадра
+    def update(self):
+        self.current_duration -= 1
+
+    # Стартует длительность этого кадра
+    def start(self):
+        self.current_duration = self.max_duration
+
+
+# Класс для создания каких либо действий персонажа. P.s. Sprites это неизменяемый массив, а значит минимальное кол-во
+# спрайтов должно быть хотя бы 2, если действие использует лишь один спрайт, то дублируйте его.
+class PersAction:
+    # длительность в секундах если что
+    def __init__(self, impulse_x, impulse_y, duration, sprites: tuple):
+        self.impulse_x = impulse_x
+        self.impulse_y = impulse_y
+        self.max_duration = duration / (1 / FPS)
+        self.current_duration = 0
+        self.sprites = sprites
+        self.current_sprite = sprites[0]
+        self.current_sprite.start()
+
+    def update(self):
+        if self.current_sprite.current_duration > 0:
+            self.current_sprite.update()
+        else:
+            for sprite in self.sprites:
+                if sprite.current_duration > 0:
+                    self.current_sprite = sprite
+        self.current_duration -= 1
+        return self.current_sprite.sprite
+
+    def start(self):
+        self.current_duration = self.max_duration
+        return self.impulse_x, self.impulse_y, self.current_sprite
+
+
 class Person:
-    def __init__(self, start_x, start_y):
+    def __init__(self, start_x, start_y, action_list: tuple):
         self.x = start_x
         self.y = start_y
         self.width = 200
@@ -16,55 +62,43 @@ class Person:
         """
         self.current_sprite = None
         self.pers = canvas.create_image(self.x, self.y, anchor='nw', image=self.current_sprite)
-        self.change_current_sprite(r"sprites\MoxStand.png")
 
         # Все, что связано с действиями персонажа
         # массив нужен для случайного выбора действия
-        self.action_list = ["moveRight", "moveLeft", "jumpRight", "jumpLeft"]
+        self.action_list = action_list
         self.action = None
-        self.action_count = 0
         self.impulse_x = 0
         self.impulse_y = 0
-
-    # Создает объект картинки по строке пути картинки
-    def change_current_sprite(self, path):
-        self.current_sprite = ImageTk.PhotoImage((Image.open(path).resize((self.width, self.height))))
-        canvas.itemconfigure(self.pers, image=self.current_sprite)
 
     # Она возвращает координаты нашего персонажа
     def get_cords(self):
         return self.x, self.x + self.width, self.y, self.y + self.height
-
-    # Она возвращает скорость нашего персонажа по оси х и у
-    def get_speed(self):
-        return self.impulse_x, self.impulse_y
 
     def set_impulse(self, impulse_x, impulse_y):
         self.impulse_y = impulse_y
         self.impulse_x = impulse_x
 
     # Смещение персонажа направо, налево и по вертикали
-    def move(self, direction, speed):
-
+    def move(self, direction: str, impulse):
         # перемещение по горизонтали
         if (direction == "horizontal" and
-                ((speed > 0 and self.x + self.width + speed <= max_x) or (speed < 0 and self.x + speed >= 1))):
-            self.x += speed
+                ((impulse > 0 and self.x + self.width + impulse <= max_x) or (impulse < 0 and self.x + impulse >= 1))):
+            self.x += impulse
         # доводит положение персонажа до правого края, но не дает выйти за него
-        elif direction == "horizontal" and self.x + self.width + speed > max_x:
+        elif direction == "horizontal" and self.x + self.width + impulse > max_x:
             self.x = max_x - self.width
         # то же самое что выше, но только с левым краем
-        elif direction == "horizontal" and self.x + speed <= 1:
+        elif direction == "horizontal" and self.x + impulse <= 1:
             self.x = 1
 
         # перемещение по вертикали
-        if direction == "vertical" and self.y + self.height + speed <= max_y:
-            self.y += speed
+        if direction == "vertical" and self.y + self.height + impulse <= max_y:
+            self.y += impulse
         # доводит положение персонажа до нижнего края, но не дает выйти за него, за верхний выходить можно
-        elif direction == "vertical" and self.y + self.height + speed > max_y:
+        elif direction == "vertical" and self.y + self.height + impulse > max_y:
             self.y = max_y - self.height
         else:
-            return 'Direction can accept only "right"/"left/vertical" arguments'
+            return 'Direction can accept only "horizontal/vertical" arguments'
 
         canvas.coords(self.pers, self.x, self.y)
 
@@ -92,58 +126,22 @@ class Person:
             self.impulse_x = 0
 
 
-# набор пресетов для движения
-def pers_action(action):
-    action_weight = 0
-    match action:
-        case "moveLeft":
-            mox.change_current_sprite("sprites/MoxLeft.png")
-            mox.set_impulse(-5, 0)
-            action_weight = 5
-        case "moveRight":
-            if mox.action_count % 3 == 0:
-                mox.change_current_sprite("sprites/MoxRight_stepOne.jpg")
-            else:
-                mox.change_current_sprite("sprites/MoxRight_stepTwo.jpg")
-            mox.set_impulse(5, 0)
-            action_weight = 5
-        case "jumpLeft":
-            mox.change_current_sprite("sprites/MoxLeft.png")
-            mox.set_impulse(-5, -15)
-            action_weight = 100
-            win.after(100)
-        case "jumpRight":
-            mox.change_current_sprite("sprites/MoxRight.png")
-            mox.set_impulse(5, -15)
-            mox.action_count = 200
-            win.after(100)
-        case "idle":
-            mox.change_current_sprite("sprites/MoxStand.png")
-            mox.set_impulse(0, 0)
-            action_weight = 2
-    mox.action_count -= action_weight
-
-
 def update():
-    x_speed, y_speed = mox.get_speed()
-    mox.move("vertical", y_speed)
-    mox.move("horizontal", x_speed)
+
+    mox.move("vertical", mox.impulse_y)
+    mox.move("horizontal", mox.impulse_x)
     mox.inertia()
     x1, x2, y1, y2 = mox.get_cords()
 
     # Здесь будут происходить события с нашим персонажем, если он стоит на земле
     if y2 >= max_y:
-        if mox.action_count <= 0:
+        # Если действие для персонажа еще не задано, или оно закончилось, то задаем новое
+        if mox.action is None or mox.action.current_duration == 0:
             mox.action = random.choice(mox.action_list)
-            mox.action_count = random.randint(300, 500)
-            win.after(1000 // FPS, update)
-
-        # Делает так, чтобы персонаж стоял на месте между действиями.
-        elif mox.action_count <= 200:
-            pers_action("idle")
+            mox.impulse_x, mox.impulse_y, mox.current_sprite = mox.action.start()
             win.after(1000 // FPS, update)
         else:
-            pers_action(mox.action)
+            mox.current_sprite = mox.action.update()
             win.after(1000 // FPS, update)
     else:
         win.after(1000 // FPS, update)
@@ -163,11 +161,43 @@ win.state('zoomed')
 win.wm_attributes("-topmost", True)
 win.wm_attributes("-transparentcolor", "white")
 
-# Создаем персонажа на определенных координатах
-mox = Person(500, 1)
+# Инициализируем объект mox из класса Person, с начальными координатами и набором действий
+mox = Person(500, 0, ())
+# набор стандартных движений
+move_right = PersAction(5, 0, 3, (
+        Sprite("sprites/MoxRight_1.jpg", 1, mox.width, mox.height),
+        Sprite("sprites/MoxRight_2.jpg", 1, mox.width, mox.height)
+    )
+)
+
+move_left = PersAction(-5, 0, 3, (
+        Sprite("sprites/MoxLeft.png", 1, mox.width, mox.height),
+        Sprite("sprites/MoxLeft.png", 1, mox.width, mox.height)
+    )
+)
+
+jump_right = PersAction(5, -15, 1, (
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height),
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height)
+    )
+)
+
+jump_left = PersAction(-5, -15, 1, (
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height),
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height)
+    )
+)
+
+idle = PersAction(0, 0, 5, (
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height),
+        Sprite("sprites/MoxStand.png", 1, mox.width, mox.height)
+    )
+)
+
+# Суем наши действия в сам объект mox
+mox.action_list = (move_right, move_left, jump_right, jump_left)
 
 # Зацикливаем нашу программу с фиксированным FPS
 if __name__ == '__main__':
     win.after(1000 // FPS, update)
     win.mainloop()
-
